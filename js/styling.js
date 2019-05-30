01b9554c09c3c8bf70068fee14254f5316858caa
Jerónimo Yepes <jeronimo_y_g@hotmail.com>
2019-05-29 23:13:43 -0500

    finalizada estructura y contenidos, comenzando estilos

---
+++ js/styling.js
@@ -0,0 +1,9 @@
+/* function $(string) {
+    return document.querySelector(string);
+} 
+
+$("section").style.height = window.innerHeight.toString() + "px";
+
+console.log(window.innerHeight.toString());
+*/
+
