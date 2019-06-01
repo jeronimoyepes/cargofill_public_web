957b552ec239e0b8faeafda58a7d6de3fd062a83
Jerónimo Yepes <jeronimo_y_g@hotmail.com>
2019-05-31 21:31:11 -0500

    Agregado el slider para oportunidades

--- js/styling.js
+++ js/styling.js
@@ -1,9 +1,55 @@
-/* function $(string) {
-    return document.querySelector(string);
-} 
+function $(selector) {
+    return document.querySelector(selector);
+};
 
-$("section").style.height = window.innerHeight.toString() + "px";
+function assingHeight (string) {
+    $(string).style.height = window.innerHeight.toString() + "px";
+}
 
-console.log(window.innerHeight.toString());
-*/
+var winWidht = window.innerWidth;
+
+if (winWidht >= 1024) {
+    assingHeight(".what");
+    assingHeight(".how");
+    assingHeight(".why");
+    assingHeight(".dev");    
+    assingHeight(".more");
+}
+
+var genBtn = $("#genBtn");
+var transBtn = $("#transBtn");
+var almaBtn = $("#almaBtn");
+var carouselContent = $(".carousel .content");
+var contentMaxHeight = $(".carousel .gene").clientHeight + "px";
+carouselContent.style.height = contentMaxHeight;
+
+function removeLiClass(params) {
+    var li = document.querySelectorAll(".carousel .nav li");
+    
+    for (i = 0; i < li.length; ++i) {
+        li[i].className = ""
+    }
+}
+
+genBtn.onclick = function () {
+    $(".content").style["margin-left"] = "0px";
+    this.className = "active";
+    removeLiClass();
+}
+
+transBtn.onclick = function () {
+    $(".content").style["margin-left"] = "-100%";
+    this.className = "active";
+    removeLiClass();    
+}
+
+almaBtn.onclick = function () {
+    $(".content").style["margin-left"] = "-200%";
+    this.className = "active";
+    removeLiClass();    
+}
+
+
+
+
 
